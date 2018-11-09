21e4830f4d0a268f4eac5fbdf168df6e3453d197
Luis Rivas <lrivas@nic.ec>
2018-11-09 16:07:35 -0500

    Cambio la plantilla Welcome.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Kurrencies Changes
+                    Kurrencies Change
                 </div>
             </div>
         </div>
