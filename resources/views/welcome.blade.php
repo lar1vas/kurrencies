313bdff113b97bf8fa469c6a5f2ca322d63f607f
Luis Rivas <lrivas@nic.ec>
2018-11-11 00:43:50 -0500

    Se agregan simbolos al mensaje de inicio.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Kurrencies Change
+                    ~ Kurrencies Change ~
                 </div>
 
                 <div class="links">
