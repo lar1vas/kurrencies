bef734dcdd334704c9b59ca0d451cfa4ca32ed90
Luis Rivas <lrivas@nic.ec>
2018-11-10 23:55:58 -0500

    Especificacion a UTF-8 de la tabla en la BD.

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,7 +14,7 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->charset = 'utf8';
+            $table->engine = 'InnoDB';
             
             $table->string('email')->index();
             $table->string('token');
