c4fe3a3a1875cb1162ea2091a127befd4fd0ee98
Luis Rivas <lrivas@nic.ec>
2018-11-18 20:56:34 -0500

    Se editar la Migracion de la tabla Users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('celular')->required();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->required();
             $table->rememberToken();
             $table->timestamps();
         });
